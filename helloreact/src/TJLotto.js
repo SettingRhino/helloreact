01ccbe5fbf730cd2b18e90b67b83a80176f47fba
settingrhino <tjtj54441@gmail.com>
2020-08-17 17:36:00 +0900

    배열로 컴포넌트 보내기

--- helloreact/src/TJLotto.js
+++ helloreact/src/TJLotto.js
@@ -1,7 +1,9 @@
 import React, {useState} from "react"
+import TJLottoBall from "./TJLottoBall"
 
 const TJLotto = (props)=>{
-    const [lottonumbers,setLottoNumbers]= useState([])
+  //  const [lottonumbers,setLottoNumbers]= useState([])
+    const [lottoball,setLottoball]=useState([])
   //  const [lottoAPI,setLottoAPI]=useState("")
     const handlesearchlotto=(e)=>{
         const templottos=[]
@@ -11,37 +13,16 @@ const TJLotto = (props)=>{
                 templottos.push(n)
             }
         }
-        setLottoNumbers(templottos.concat())
+        setLottoball(templottos.map((num)=>{
+          return(<TJLottoBall lottonumber={num}/>)
+        }).concat())
     }
-  /*  const  callApi = (e) => {
-        e.preventDefault()
-        //
-        fetch("https://www.dhlottery.co.kr/common.do?method=getLottoNumber&drwNo=903",{
-          mode: 'cors',
-          headers:{
-            'Access-Control-Allow-Origin':'*'
-          }
-        })
-            .then(res => res.json())
-            .then(json => {
-                setLottoAPI(json.toString())
-            })
-    }*/
-
-
 
     return (//ListItems를 하위로 만들어준다.=>props로 배열을 넘겨주면 ListItem은 Item을 그만큼 만들어줌
-    <div>
-        <div>{lottonumbers[0]}</div>
-        <div>{lottonumbers[1]}</div>
-        <div>{lottonumbers[2]}</div>
-        <div>{lottonumbers[3]}</div>
-        <div>{lottonumbers[4]}</div>
-        <div>{lottonumbers[5]}</div>
-        <div>{lottonumbers[6]}</div>
+        <div>
         <button onClick={handlesearchlotto}>조회</button>
-
-    </div>
-)
+        <div>{lottoball}</div>
+        </div>
+      )
 }
 export default TJLotto
---
+++ helloreact/src/TJLottoBall.js
@@ -0,0 +1,9 @@
+import React from "react"
+
+const TJLottoBall =(props)=>{
+  return (
+    <div>{props.lottonumber}</div>
+  )
+}
+
+export default TJLottoBall
