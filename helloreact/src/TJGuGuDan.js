c1143d01be1809832059d36a365d7ab677ac22a9
settingrhino <tjtj54441@gmail.com>
2020-08-17 18:33:48 +0900

    css 추가!

--- helloreact/src/TJGuGuDan.js
+++ helloreact/src/TJGuGuDan.js
@@ -1,8 +1,9 @@
 import React from "react"
 
+
 const TJGuGuDan=(props)=>{
   return(
-    <div>
+    <div className='TJGuGuDan'>
     구구단을 만들자~~
     </div>
   )
--- helloreact/src/Taejune.js
+++ helloreact/src/Taejune.js
@@ -1,6 +1,7 @@
 import React from "react"
 import TJLotto from "./TJLotto";
 import TJGuGuDan from "./TJGuGuDan";
+import "./TJStyle.css"
 const Taejune=(props)=>{
     return(
         <div>
